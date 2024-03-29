1e212b045ca6b1d421615be3575c4a68d1ca93a5
abishek <skabishek9576@gmail.com>
2024-03-29 14:50:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "../src/image-qr-code.png";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="new">
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="para">
+          <span className="imp">Improve your front-end</span> <br /> skills by building projects
+          </h1>
+          
+            <p className="skills">
+              Scan the QR code to visit Frontend
+              <br />
+              Mentor and take your coding skills to <br />
+             <span className="the"> the next level</span> 
+            </p>
+          
+        </header>
+      </div>
     </div>
   );
 }
